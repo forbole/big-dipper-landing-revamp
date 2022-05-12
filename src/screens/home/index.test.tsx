f22064765aa55a6d20a5a7591a4c54b11a9deac6
yayay927 <isabelleya927@gmail.com>
2022-05-12 15:38:30 +0200

    update path

---
+++ src/screens/home/index.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '.';
+
+// ==================================
+// mocks
+// ==================================
+const mockI18n = {
+  t: (key: string) => key,
+  lang: 'en',
+};
+
+jest.mock('next-translate/useTranslation', () => () => mockI18n);
+
+// ==================================
+// unit tests
+// ==================================
+describe('Dummy Test', () => {
+  it('matches snapshot', async () => {
+    const { asFragment } = render(<Home />);
+    expect(screen.getByText(
+      /Testing Next.js With Jest and React Testing Library/i,
+    )).toBeInTheDocument();
+    expect(screen.getByText(
+      /hwohoh/i,
+    )).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});

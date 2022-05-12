b0574bea1bb913ff9cf7bd0a396de1d6fcb6ee2a
yayay927 <isabelleya927@gmail.com>
2022-05-12 15:23:20 +0200

    test on i18n

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -2,6 +2,8 @@ import useTranslation from 'next-translate/useTranslation';
 
 const Home = () => {
   const { t } = useTranslation('common');
+  console.log(t('hola'));
+  console.log(t('test'));
 
   return (
     <>

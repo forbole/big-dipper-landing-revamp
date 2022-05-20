549ceff1b926d593f28a53289529098f82b0a528
teamchong <25894545+teamchong@users.noreply.github.com>
2022-05-20 15:23:32 -0400

    Update/testing (#4)

---
+++ src/screens/donation/index.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MockTheme } from '@mocks';
+import Component from '.';
+
+const mockI18n = {
+  t: (key: string) => key,
+  lang: 'en',
+};
+jest.mock('next-translate/useTranslation', () => () => mockI18n);
+jest.mock('@components', () => ({
+  Layout: (props: any) => <div data-testid="Layout" {...props} />,
+  SectionBox: (props: any) => <div data-testid="SectionBox" {...props} />,
+  SectionLimit: (props: any) => <div data-testid="SectionLimit" {...props} />,
+  ContentBox: (props: any) => <div data-testid="ContentBox" {...props} />,
+}));
+// ==================================
+// unit tests
+// ==================================
+describe('Donation', () => {
+  it('matches snapshot', async () => {
+    const { container } = render(
+      <MockTheme>
+        <Component />
+      </MockTheme>,
+    );
+
+    expect(screen.getByText(
+      /donation/i,
+    )).toBeInTheDocument();
+    expect(screen.getByText(
+      /description/i,
+    )).toBeInTheDocument();
+    expect(screen.getByTestId('Layout')).toBeInTheDocument();
+    expect(screen.getByTestId('SectionBox')).toBeInTheDocument();
+    expect(screen.getByTestId('SectionLimit')).toBeInTheDocument();
+    expect(screen.getAllByTestId('ContentBox').length).toBe(3);
+    expect(container).toMatchSnapshot();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
--- tests/mocks/svg.js
+++ tests/mocks/svg.js
@@ -1,2 +1,6 @@
-export default 'SvgrURL';
-export const ReactComponent = 'div';
+import React from 'react';
+
+const SvgrMock = React.forwardRef((props, ref) => <span ref={ref} {...props} />);
+
+export const ReactComponent = SvgrMock;
+export default SvgrMock;

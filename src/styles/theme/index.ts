0b68dd60652a326213fccc407585d97b96e35552
Wingman <ryuash123@gmail.com>
2022-05-20 09:41:21 -0400

    add: font weights

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -18,23 +18,31 @@ export const common: ThemeOptions = {
     },
   },
   typography: {
+    fontFamily: `-apple-system, BlinkMacSystemFont,"Segoe UI","Roboto",
+    "Oxygen","Ubuntu","Helvetica Neue", Arial, sans-serif`,
     h1: {
       fontSize: '2.5rem',
+      fontWeight: 600,
     },
     h2: {
       fontSize: '2rem',
+      fontWeight: 600,
     },
     h3: {
       fontSize: '1.5rem',
+      fontWeight: 600,
     },
     h4: {
       fontSize: '1.25rem',
+      fontWeight: 600,
     },
     h5: {
       fontSize: '1.125rem',
+      fontWeight: 600,
     },
     h6: {
       fontSize: '1rem',
+      fontWeight: 600,
     },
     body1: {
       fontSize: '1rem',
@@ -48,6 +56,7 @@ export const common: ThemeOptions = {
     button: {
       fontSize: '1rem',
       textTransform: 'none',
+      fontWeight: 600,
     },
   },
   mixins: {

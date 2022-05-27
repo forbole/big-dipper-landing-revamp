3ffa6e1e97be5d1267e1d0582252ade830dfbd20
teamchong <25894545+teamchong@users.noreply.github.com>
2022-05-27 09:46:14 -0400

    Footer (#18)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@
 - Add About page
 - Add Donation page
 - Add FAQ page
+- Add Footer
---
+++ src/components/footer/types.ts
@@ -0,0 +1,11 @@
+export type LinkType = {
+  key: string;
+  url: string;
+  external?: boolean;
+}
+
+export type IconType = {
+  component: React.ReactNode;
+  className: string;
+  url: string;
+}

4fa5f95b0cf97154ffc6f05904bc75f6f8272c04
teamchong <25894545+teamchong@users.noreply.github.com>
2022-05-31 15:05:06 -0400

    setup: tnc

--- src/screens/terms_and_conditions/utils.ts
+++ src/screens/terms_and_conditions/utils.ts
@@ -1,36 +1,3 @@
-// export const items = [
-//   {
-//     topic: 'affiliate',
-//   },
-//   {
-//     topic: 'country',
-//   },
-//   {
-//     topic: 'company',
-//   },
-//   {
-//     topic: 'device',
-//   },
-//   {
-//     topic: 'feedback',
-//   },
-//   {
-//     topic: 'service',
-//   },
-//   {
-//     topic: 'termsAndConditionsDetail',
-//   },
-//   {
-//     topic: 'third-partySocialMediaService',
-//   },
-//   {
-//     topic: 'website',
-//   },
-//   {
-//     topic: 'you',
-//   },
-// ];
-
 export const definitions = [
   {
     key: 'Affiliate',

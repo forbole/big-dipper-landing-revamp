1e9b703ff2139c813b951a08e935ee9bbeba5b0b
teamchong <25894545+teamchong@users.noreply.github.com>
2022-06-02 15:03:58 -0400

    add: background props

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,6 +10,9 @@ const Layout: React.FC<LayoutProps & ComponentDefault> = (props) => {
   const classes = useStyles();
   return (
     <main className={classes.root}>
+      <img className="back back-1" src="/images/background/back-1.svg" alt="back-1" />
+      <img className="back back-2" src="/images/background/back-2.svg" alt="back-2" />
+      <img className="back back-3" src="/images/background/back-3.svg" alt="back-3" />
       <Nav />
       <div className={classes.content}>
         {props.children}

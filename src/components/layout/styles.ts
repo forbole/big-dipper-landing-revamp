934dc74a55d039b788455906dfe5d73860c18b94
Wingman <ryuash123@gmail.com>
2022-06-02 15:37:09 -0400

    .

--- src/components/layout/styles.ts
+++ src/components/layout/styles.ts
@@ -9,22 +9,6 @@ export const useStyles = () => {
           flexDirection: 'column',
           minHeight: '100vh',
           ...theme.mixins.gradientBackground,
-          position: 'relative',
-          overflow: 'hidden',
-          '& .back': {
-            position: 'absolute',
-            width: '100%',
-            height: 'auto',
-          },
-          '& .back-1': {
-            top: 0,
-          },
-          '& .back-2': {
-            top: '800px',
-          },
-          '& .back-3': {
-            bottom: 0,
-          },
         },
         content: {
           flex: 1,

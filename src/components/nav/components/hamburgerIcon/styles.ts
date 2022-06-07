1d192337e9a3d287ce8f5b0cda6edb18d61d5a2d
yayay927 <isabelleya927@gmail.com>
2022-06-07 17:25:41 +0100

    update

--- src/components/nav/components/hamburgerIcon/styles.ts
+++ src/components/nav/components/hamburgerIcon/styles.ts
@@ -4,60 +4,58 @@ export const useStyles = () => {
   const styles = makeStyles(
     (theme) => {
       return ({
-        root: {
-          // padding: theme.spacing(2, 1),
-          // '& .wrapper': {
-          //   margin: 'auto',
-          //   maxWidth: '1024px',
-          //   textAlign: 'center',
-          //   height: '60px',
-          //   alignItems: 'center',
-          // },
-          // '& .logo': {
-          //   height: '36px',
-          //   margin: '0 auto',
-          //   display: 'inline-block',
-          // },
-          '& .hamburgerMenu': {
-            height: '60px',
-            float: 'right',
-          },
-          '& .burger-menu .burger-menu.open': {
-            display: 'inline-block',
-            cursor: 'pointer',
-            position: 'fixed',
-            right: '20px',
-            bottom: '40px',
-            zIndex: '9999',
-            background: '#fff',
-            padding: '10px',
-            borderRadius: '25px',
-            boxShadow: '0 1px 4px rgba(0, 0, 0, 0.3)',
-          },
-          '& .burger-menu .bar1 .bar2 .bar3': {
-            width: '25px',
-            height: '3px',
-            backgroundColor: '#333',
-            margin: '4px 0',
-            transition: '0.4s',
-          }
-
-
-
-'& .burger-menu.open .bar1': {
-  -webkitTransform: 'rotate(-45deg) translate(-4px, 4px)',
-  transform: 'rotate(-45deg) translate(-4px, 4px)',
-}
-
-.burger-menu.open .bar2 {
-  opacity: 0;
-}
-
-.burger-menu.open .bar3 {
-  -webkit-transform: rotate(45deg) translate(-6px, -6px);
-  transform: rotate(45deg) translate(-6px, -6px);
-}
+        // root: {
+        // height: '60px',
+        // position: 'absolute',
+        // right: theme.spacing(0),
+        // padding: theme.spacing(2, 1),
+        // '& .wrapper': {
+        //   margin: 'auto',
+        //   maxWidth: '1024px',
+        //   textAlign: 'center',
+        //   height: '60px',
+        //   alignItems: 'center',
+        // },
+        // '& .burger-menu': {
+        //   height: '60px',
+        //   width: '60px',
+        //   float: 'right',
+        // },
+        '& .burger-menu, .burger-menu.open': {
+          height: '60px',
+          width: '60px',
+          position: 'absolute',
+          right: theme.spacing(0),
+          display: 'inline-block',
+          cursor: 'pointer',
+          // position: 'fixed',
+          // right: '20px',
+          // bottom: '40px',
+          zIndex: '9999',
+          background: '#fff',
+          padding: '10px',
+          // borderRadius: '25px',
+          // boxShadow: '0 1px 4px rgba(0, 0, 0, 0.3)',
         },
+        '& .bar1, .bar2, .bar3': {
+          width: '25px',
+          height: '3px',
+          backgroundColor: '#333',
+          margin: '4px 0',
+          transition: '0.4s',
+        },
+        // '& .burger-menu.open, .bar1': {
+        //   // -webkitTransform: 'rotate(-45deg) translate(-4px, 4px)',
+        //   transform: 'rotate(-45deg) translate(-4px, 4px)',
+        // },
+        // '& .burger-menu.open, .bar2': {
+        //   opacity: '0',
+        // },
+        // '& .burger-menu.open, .bar3': {
+        //   // -webkit-transform: rotate(45deg) translate(-6px, -6px);
+        //   transform: 'rotate(45deg) translate(-6px, -6px)',
+        // },
+        // },
       });
     },
   )();

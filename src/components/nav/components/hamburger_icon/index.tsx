d3408a6b8594c9ed88a44c32a228d91caf58a513
yayay927 <isabelleya927@gmail.com>
2022-06-09 00:49:37 +0100

    import toggle onclick

--- src/components/nav/components/hamburger_icon/index.tsx
+++ src/components/nav/components/hamburger_icon/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useStyles } from './styles';
+import { toggleHamburgerMenu } from '../../hooks';
 
 const HamburgerIcon = (props: any) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div
+      className={classes.root}
+      onClick={toggleHamburgerMenu}
+    >
       <div className={!props ? 'burger-menu open' : 'burger-menu'}>
         <div className="bar1" key="b1" />
         <div className="bar2" key="b2" />

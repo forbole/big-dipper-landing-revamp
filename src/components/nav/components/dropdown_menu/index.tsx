58fff6528f2e99806d0a373a83f2aa730803f1b0
yayay927 <isabelleya927@gmail.com>
2022-06-08 16:33:10 +0100

    add dropdown menu part

---
+++ src/components/nav/components/dropdown_menu/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useStyles } from './styles';
+
+const DropdownMenu = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root} />
+  );
+};
+
+export default DropdownMenu;
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -3,7 +3,8 @@ import BDLogo from '@assets/big-dipper-red.svg';
 import HamburgerMenu from '@assets/hamburger-menu.svg';
 import { SectionLimit } from '@components';
 import { useStyles } from './styles';
-import HamburgerIcon from './components/hamburgerIcon';
+import HamburgerIcon from './components/hamburger_icon';
+import DropdownMenu from './components/dropdown_menu';
 
 const Nav = () => {
   const classes = useStyles();
@@ -14,6 +15,7 @@ const Nav = () => {
           <BDLogo className="logo" />
           <HamburgerMenu className="hamburgerMenu" />
           <HamburgerIcon open />
+          <DropdownMenu />
         </div>
       </SectionLimit>
     </div>

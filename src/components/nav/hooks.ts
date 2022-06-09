bd50c2080e567f7b25dafbc528a885952255c8e9
yayay927 <isabelleya927@gmail.com>
2022-06-09 19:13:30 +0100

    add onnclick and func

--- src/components/nav/hooks.ts
+++ src/components/nav/hooks.ts
@@ -15,14 +15,17 @@ export const useNav = () => {
   };
 
   const toggleHamburgerMenu = () => {
+    console.log('enter toggleHamburgerMenu fucntion');
     // click hamburger icon to open modal and change hamburger menu to show "X"
-    if ((state.isIcon) || (state.isMenu)) {
+    if ((state.isIcon) || (!state.isMenu)) {
+      console.log('situation 1');
       handleSetState({
         isIcon: false,
         isMenu: true,
       });
     } else {
       // click hamburger icon to close modal and recover hamburger munu to default
+      console.log('situation 2');
       handleSetState({
         isIcon: true,
         isMenu: false,

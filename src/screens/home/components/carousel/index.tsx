e423728cbb791ea3ccbba9604a696c3ea63e795b
yayay927 <isabelleya927@gmail.com>
2022-07-07 17:09:05 +0800

    create utils and update paper to card mui

--- src/screens/home/components/carousel/index.tsx
+++ src/screens/home/components/carousel/index.tsx
@@ -3,15 +3,29 @@ import {
 } from '@components';
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { Paper, Button } from '@mui/material';
+import { Button, Card } from '@mui/material';
 import { useStyles } from './styles';
+import { carouselData } from './utils';
 
 const CarouselComponent = () => {
   const classes = useStyles();
 
   return (
     <SectionLimit className={classes.root}>
-      <h1>Carousel</h1>
+      <div>
+        <Carousel>
+          {carouselData.map((item) => (
+            <Card key={item.image}>
+              <h2>{item.key}</h2>
+              <p>{item.details}</p>
+
+              <Button className="CheckButton">
+                Check it out!
+              </Button>
+            </Card>
+          ))}
+        </Carousel>
+      </div>
     </SectionLimit>
   );
 };
---
+++ src/screens/home/components/carousel/utils.ts
@@ -0,0 +1,22 @@
+export const carouselData = [
+  {
+    key: 'Real-time Interchain Explorer',
+    details: 'View real-time status of different networks',
+    image: '1',
+  },
+  {
+    key: 'Real-time Interchain Explorer',
+    details: 'View real-time status of different networks',
+    image: '2',
+  },
+  {
+    key: 'Real-time Interchain Explorer',
+    details: 'View real-time status of different networks',
+    image: '3',
+  },
+  {
+    key: 'Real-time Interchain Explorer',
+    details: 'View real-time status of different networks',
+    image: '4',
+  },
+];
